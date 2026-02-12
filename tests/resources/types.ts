ee8a746d809757d55f9991f8eaee604255865897
sircje <connorjeischen@gmail.com>
2026-02-12 16:54:58 -0600

    cleanup

--- tests/resources/types.ts
+++
@@ -1,7 +0,0 @@
-export interface TestCase {
-    name: string;
-    project: "Web Application" | "Mobile Application";
-    task: string;
-    column: "To Do" | "In Progress" | "Done";
-    tags: string[];
-}
